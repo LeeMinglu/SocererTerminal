2f959931e5e6681b47433ad3699e68455c0107fb
LeeMinglu <402007353@qq.com>
2015-11-23 12:50:43 +0800

    初始化程序

---
+++ SocererTerminal/SocererTerminal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SocererTerminal
+//
+//  Created by 李明禄 on 15/11/23.
+//  Copyright © 2015年 SocererGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SocererTerminal/SocererTerminal/Classes/Ad/Controller/MLAdViewController.h
@@ -0,0 +1,13 @@
+//
+//  MLAdViewController.h
+//  SocererTerminal
+//
+//  Created by 李明禄 on 15/11/23.
+//  Copyright © 2015年 SocererGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MLAdViewController : UIViewController
+
+@end
